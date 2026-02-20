ce5f5b3aeff60539a5e80367d09404c74111f0b3
Alwaysnyzzz <masdenys34@gmail.com>
2026-02-21 05:39:48 +0700

    upload website ke repo baru

---
+++ config.js
@@ -0,0 +1,10 @@
+const CONFIG = {
+    API_BASE_URL: 'https://brat.siputzx.my.id',
+    APP_NAME: 'KEI ASSISTENT',
+    DASHBOARD_IMAGE_URL: 'https://files.catbox.moe/olvb6d.jpg', // Ganti sesuai keinginan
+    ALAT_IMAGE_URL: 'https://files.catbox.moe/olvb6d.jpg',      // Bisa sama atau beda
+    WA_NUMBER: '+62 877-1347-2756',
+    WA_LINK: 'https://wa.me/6287713472756',
+    TELEGRAM_USERNAME: '@keikawaiii',
+    TELEGRAM_LINK: 'https://t.me/keikawaiii'
+};
